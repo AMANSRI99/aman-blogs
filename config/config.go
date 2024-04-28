7957dd67de580a2bd40ee72d8631d6d13ad6be51
AMANSRI99 <amansri99@gmail.com>
2024-04-28 14:30:39 +0530

    added provision to load env files using godotenv

--- config/config.go
+++ config/config.go
@@ -1,10 +1,23 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/AMANSRI99/aman-blogs/utils/errs"
+	"github.com/joho/godotenv"
+)
 
 const (
-	PORT = "APP_PORT"
+	PORT     = "APP_PORT"
+	ENV_FILE = ".env"
 )
+
+func init() {
+	if err := godotenv.Load(ENV_FILE); err != nil {
+		log.Fatal(errs.Wrap(err))
+	}
+}
 
 func Get(key string) string {
 	return os.Getenv(key)
