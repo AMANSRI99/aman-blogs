07751dff10fefdb6a87684347e9150fae43f6926
AMANSRI99 <amansri99@gmail.com>
2024-11-01 11:18:40 +0530

    added validation for database connection

--- db/database.go
+++ db/database.go
@@ -3,18 +3,33 @@ package db
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 )
 
 var database *gorm.DB
 
 // init() functions are special - they are automatically called when the package is initialized, before the main() function runs.
 func init() {
-	dsn := "user=postgres.xqenmhuzhqykuhugwchp password=<password> host=aws-0-ap-south-1.pooler.supabase.com port=6543 dbname=postgres"
+	dsn := "user=postgres.xqenmhuzhqykuhugwchp password=qf34n6xd@smMF@i host=aws-0-ap-south-1.pooler.supabase.com port=6543 dbname=postgres"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	database = db
+
+	// Test connection
+    sqlDB, err := db.DB()
+    if err != nil {
+        log.Fatalf("Failed to get database instance: %v", err)
+    }
+
+    // Test the connection
+    err = sqlDB.Ping()
+    if err != nil {
+        log.Fatalf("Failed to ping database: %v", err)
+    }
+
+    log.Println("Successfully connected to database")
+    database = db
 }
 
 func GetDatabase() *gorm.DB {
