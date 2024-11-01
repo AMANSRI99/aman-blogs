fa004caf12acf339908617bdec64320738239e4a
AMANSRI99 <amansri99@gmail.com>
2024-11-01 11:17:46 +0530

    added middleware for error logging

--- main.go
+++ main.go
@@ -12,12 +12,18 @@ import (
 	"github.com/AMANSRI99/aman-blogs/template"
 	"github.com/AMANSRI99/aman-blogs/utils/routing"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	println("welcome to my nook of the internet")
 
 	e := echo.New()
+
+	// Adding error logging middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	routing.SetupRouter(e)
 	e.Static("/dist", "dist")
 
@@ -27,8 +33,11 @@ func main() {
 		"public/admin/components/*.html",
 		"public/blog/*.html", // Add blog templates
 	)
-	// Initialize repository
-	postRepo := repository.NewPostRepository(db.GetDatabase())
+	// Initializing db connection
+	dbConnection := db.GetDatabase()
+
+	// Initializing repository
+	postRepo := repository.NewPostRepository(dbConnection)
 
 	// Public routes
 	e.GET("/", func(c echo.Context) error {
@@ -37,14 +46,14 @@ func main() {
 
 	// Blog routes
 	blogHandler := public.NewBlogHandler(postRepo) // This creates a handler that will manage blog-related operations
-	// It gets postRepo so it can perform database operations
+	// It gets postRepo so it can perform database operations. This is called repository pattern.
 	blogGroup := e.Group("/blog") // This creates /blog/* routes
 	blogHandler.Register(blogGroup)
 
 	// Admin routes
 	adminGrp := e.Group("admin")
 	admin.NewAdminFrontend(adminGrp, postRepo)
-	admin.NewPostfrontend(adminGrp, postRepo)
+	admin.NewPostFrontend(adminGrp, postRepo)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", config.Get(config.PORT))))
 }
